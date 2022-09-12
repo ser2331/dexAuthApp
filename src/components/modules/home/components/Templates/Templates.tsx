748895301b2d4e179c6a8ff1edde47f7bcd67492
s.dukov <dukov_serega@mail.ru>
2022-09-12 16:44:47 +0300

    some fix

--- src/components/modules/home/components/Templates/Templates.tsx
+++ src/components/modules/home/components/Templates/Templates.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Button } from 'antd';
 import { CustomContentWrapper } from '../../../../common/components/CustomContentWrapper/CustomContentWrapper';
 import { useTranslation } from 'react-i18next';
 import { Rest } from '../Rest/Rest';
 import { Entertainment } from '../Entertainment/Entertainment';
 
-const data = (
-  title: string,
-  breadcrumb: string[],
-  tabs: string[],
-  buttonTitle: string,
-  addTemplates: () => void
-) => {
+const data = (title: string, breadcrumb: string[], tabs: string[]) => {
   return {
     title: title,
     breadcrumb: breadcrumb,
-    extra: (
-      <Button type='primary' style={{ right: 50 }} onClick={addTemplates}>
-        {buttonTitle}
-      </Button>
-    ),
     items: [
       {
         label: tabs[0],
@@ -48,11 +36,6 @@ export const Templates = () => {
   const title = t('templates');
   const breadcrumb = [t('documents'), t('templates')];
   const tabs = [t('schedule_relax'), t('schedule_entertainment')];
-  const buttonTitle = t('add_invoice');
 
-  const addTemplates = () => {
-    console.log('hi');
-  };
-
-  return CustomContentWrapper(data(title, breadcrumb, tabs, buttonTitle, addTemplates));
+  return CustomContentWrapper(data(title, breadcrumb, tabs));
 };
